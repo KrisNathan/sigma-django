a56168e9be116bd8efda3ba766310ed50233f199
KrisNathan <kristophernathanael2005@gmail.com>
2025-03-14 10:47:50 +0700

    feat: add app landing view

--- src/sclassdev/app_landing/views.py
+++ src/sclassdev/app_landing/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpRequest
 
 # Create your views here.
+# def index(request: HttpRequest):
+#   return HttpResponse("Hello world!")
+
+def view(request: HttpRequest):
+  return render(request, 'app_landing/index.html')
